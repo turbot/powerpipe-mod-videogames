a021099baaf90f639c2e2bc1b8fd4af850408459
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-04 20:24:39 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -3,5 +3,5 @@ mod "videogames" {
   description   = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using MySQL with Powerpipe."
   color         = "#262626"
   documentation = file("./README.md")
-  categories    = ["mySQL"]
+  categories    = ["dashboard", "mySQL"]
 }
