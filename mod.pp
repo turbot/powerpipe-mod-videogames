8512a448552bfb9596fe29d137aa18a1b0ee95d8
sourav chakraborty <sourav@turbot.com>
2024-03-06 17:05:37 +0530

    udpate

--- mod.pp
+++ mod.pp
@@ -4,10 +4,10 @@ mod "videogames" {
   color         = "#262626"
   documentation = file("./README.md")
   categories    = ["dashboard", "mysql"]
-}
 
-opengraph {
-  title       = "Powerpipe Mod for Video Games"
-  description = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
-  image       = "/images/mods/turbot/videogames-social-graphic.png"
+  opengraph {
+    title       = "Powerpipe Mod for Video Games"
+    description = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
+    image       = "/images/mods/turbot/videogames-social-graphic.png"
+  }
 }
