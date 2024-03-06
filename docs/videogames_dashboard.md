8c83f4e27e7d4347cda95292ee0b9f7bf2e2167f
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2024-03-06 18:47:57 +0530

    Add Powerpipe Dashboard page for Video Game (#1)
    Co-authored-by: souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
    Co-authored-by: misraved <ved@turbot.com>
    Co-authored-by: sourav chakraborty <sourav@turbot.com>

---
+++ docs/videogames_dashboard.md
@@ -0,0 +1,7 @@
+This dashboard answers the following questions:
+
+- How many video games are currently cataloged in the database?
+- What is the total number of game developers?
+- How many publishers are involved in the video games listed?
+- Which are the top 10 video games rated by user score, and what scores did they achieve?
+- What are the top 10 game genres by the number of games, and how many games are there in each genre?
---
+++ mod.pp
@@ -0,0 +1,13 @@
+mod "videogames" {
+  title         = "Video Games"
+  description   = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
+  color         = "#262626"
+  documentation = file("./README.md")
+  categories    = ["dashboard", "mysql"]
+
+  opengraph {
+    title       = "Powerpipe Mod for Video Games"
+    description = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
+    image       = "/images/mods/turbot/videogames-social-graphic.png"
+  }
+}
