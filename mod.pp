bd901cf35c6f32a9abcfe26878088096d9b602c6
sourav chakraborty <sourav@turbot.com>
2024-03-06 21:21:39 +0530

    update icon and image in mod.pp

--- mod.pp
+++ mod.pp
@@ -3,12 +3,12 @@ mod "videogames" {
   description   = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
   color         = "#262626"
   documentation = file("./index.md")
-  icon          = "/images/mods/turbot/videogames.svg"
+  icon          = "/images/mods/turbot/videogames-dashboard.svg"
   categories    = ["dashboard", "mysql"]
 
   opengraph {
     title       = "Powerpipe Mod for Video Games"
     description = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
-    image       = "/images/mods/turbot/videogames-social-graphic.png"
+    image       = "/images/mods/turbot/videogames-dashboard-social-graphic.png"
   }
 }
