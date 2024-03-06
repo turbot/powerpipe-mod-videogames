4e73d777dcc40cce867d905f9dae037b7ff6ce61
sourav chakraborty <sourav@turbot.com>
2024-03-06 21:00:50 +0530

    update mod.pp

--- mod.pp
+++ mod.pp
@@ -2,7 +2,7 @@ mod "videogames" {
   title         = "Video Games"
   description   = "Delve into the gaming universe with the Video Game Analysis Dashboard, showcasing comprehensive metrics including game counts, developer and publisher activities, genre popularity, and platform performance, all through dynamic SQL-driven visualizations using Powerpipe with MySQL."
   color         = "#262626"
-  documentation = file("./README.md")
+  documentation = file("./index.md")
   icon          = "/images/mods/turbot/videogames.svg"
   categories    = ["dashboard", "mysql"]
 
